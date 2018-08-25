cdbf912f16cdf0ea2e8651899f4576f894017bf7
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-08-25 22:03:48 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,15 +1,7 @@
 # require modules here
 require "yaml"
 file_path = "./lib/emoticons.yml"
-def load_library(file_path)
-  library = {"get_meaning" => {}, "get_emoticon" => {} }
-  YAML.load_file(file_path).each do |meaning, array|
-    english, japanese = array
-    library["get_emoticon"][english] = japanese
-    library["get_meaning"][japanese] = meaning
-  end
-  library
-end
+
 
 def load_library
   # code goes here
