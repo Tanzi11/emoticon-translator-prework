e2a58002e0d3702506a8a4ef8d81b4928d38cf8c
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-08-25 21:57:29 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,6 @@
 # require modules here
 require "yaml"
-
+file_path = "./lib/emoticons.yml"
 def load_library(file_path)
   library = {"get_meaning" => {}, "get_emoticon" => {} }
   YAML.load_file(file_path).each do |meaning, array|
