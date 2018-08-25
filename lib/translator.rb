bf59bf94cacc9de234eb0b7216b9ccb87d5b0d9f
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-08-25 22:13:18 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 def load_library(emoticon_file)
   emoticons = YAML.load_file('./lib/emoticons.yml')
-   emoticon_lib = {'get_meaning'  => {},
+  emoticon_lib = {'get_meaning'  => {},
                   'get_emoticon' => {} }
                   
 
