085f3f79273625040b752fe33ca7aa4e58bc9e4d
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-08-25 21:52:00 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,13 @@ require "yaml"
 def load_library(file_path)
   hash = {"get_emoticon" => {}, "get_meaning" => {} }
   YAML.load_file(file_path).each do |meaning, emot_arr|
-    
+    english = emot_arr[0]
+    japanese = emot_arr[1]
+    hash["get_meaning"][japanese] = meaning
+    hash["get_emoticon"][english] = japanese
+  end
+  hash
+end
 
 def load_library
   # code goes here
