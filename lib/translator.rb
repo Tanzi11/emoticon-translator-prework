cdff5834df0604fdaf5e2bdb797c3d594308e8fd
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-08-25 22:15:34 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,11 +15,11 @@ def load_library(emoticon_file)
 end
     
 
-def get_japanese_emoticon(file_path, emoticon)
-  emoticons = load_library(file_path)
-  emoji = emoticons.values.find {|faces| faces[0] == emoticon}
-  emoji == nil ? "Sorry, that emoticon was not found" : emoji[1]
-end
+def get_japanese_emoticon(emoticon_file, emoticon)
+  emoticon_lib = load_library(emoticon_file)
+  japanese_emoticon = emoticon_lib['get_emoticon'][emoticon]
+  japanese_emoticon ? japanese_emoticon : 'Sorry, that emoticon was not found'
+end	
 
 def get_english_meaning(file_path, emoticon)
   emoticons = load_library(file_path)
