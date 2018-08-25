f3cc191fe0fb6831345c42a6d6487eb7f9aebec6
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-08-25 21:48:54 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 # require modules here
 require 'yaml'
-
-esponse = {"get_meaning" => {}, "get_emoticon" => {}}
+def load_library(yaml_file)
+response = {"get_meaning" => {}, "get_emoticon" => {}}
   library = YAML.load_file(yaml_file)
   library.each do |trans, emos|
     response["get_meaning"][emos[1]] = trans
